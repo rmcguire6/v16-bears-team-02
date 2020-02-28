4e61f967ccf5659e3c33a93765a268ed4df6b4fc
Regina Mc Guire <rmcguire@antelecom.net>
2020-02-27 16:25:33 -0800

    Add basic meeting router

--- server/index.js
+++ server/index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const db = require('./models')
+const meetingRouter = require('./routers/meeting')
 
 db.sequelize.authenticate()
     .then(() => console.log('Database connected ...'))
     .catch(err => console.log(`Error: ${err}`))
-const app = express()
 
-app.get('/', (req, res) => {
-    res.send('backend up')
-})
+const app = express()
+app.use(meetingRouter)
 
 port = process.env.PORT || 5000
 app.listen(port, () => {
