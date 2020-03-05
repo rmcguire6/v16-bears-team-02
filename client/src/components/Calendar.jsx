34ba6cbad0e2806eb39dbaf0e9d62fa7978a5806
Rolando Dollente Jr <rdollent@ab.bluecross.ca>
2020-03-05 16:06:55 -0700

    week component prelim work. figured out how to detect current week!

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import {connect} from 'react-redux';
 import { storeCurrentMonth, storeCurrentYear, storeCurrentDate, storeCurrentWeek } from '../actions/calendarActions';
 
-import { getCurrentMonth, getCurrentYear, getCurrentDate, getFirstDay, getLastFullDate, getNumberOfWeeks } from './Functions';
+import { getCurrentMonth, getCurrentYear, getCurrentDate, getFirstDay, getLastFullDate, getNumberOfWeeks, createWeek } from './Functions';
 
 
 class Calendar extends React.Component {
@@ -20,24 +20,16 @@ class Calendar extends React.Component {
         this.populateDays = this.populateDays.bind(this);
         this.prevMonth = this.prevMonth.bind(this);
         this.nextMonth = this.nextMonth.bind(this);
-        this.initFunctions = this.initFunctions.bind(this);
         this.passRefToWeek = this.passRefToWeek.bind(this);
         this.getParentData = this.getParentData.bind(this);
         this.cloneThis = this.cloneThis.bind(this);
+        this.createWeek = createWeek.bind(this);
         this.days = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
         this.months = [
             'January', 'February', 'March', 'April',
             'May', 'June', 'July', 'August', 'September',
             'October', 'November', 'December'
         ];
-
-        // for use with week. create a reference for a single week then forward the ref to Week component for display.
-        // max possible # of weeks in any given month is 6
-        // for(let i = 1; i <= 6; i++) {
-        //     this[`week${i}ref`] = React.createRef();
-        // }
-
-        // this.myRef = React.createRef();
         
     }
 
@@ -49,20 +41,10 @@ class Calendar extends React.Component {
             this.getLastFullDate();
             this.getNumberOfWeeks();
             this.populateDates();
-            this.initFunctions();
             this.cloneThis();
+            this.createWeek();
         }
     }
-
-    // run these functions when component is loaded
-    initFunctions() {
-        console.log('init fired!');
-
-    }
-
-    // use react.createelement to dynamically add elements in page
-    // need to use this for # of weeks / tr
-    // {React.createElement('th', {}, 'created element')}
 
     populateDays() {
         console.log('populateDays');
@@ -75,138 +57,28 @@ class Calendar extends React.Component {
         );
     }
 
-    // make nested array with the weeks in it
-    // [[week1],[week2], [week3], [week4]]
-    // where week1 = ['','','','1','2','3','4']
-    // when 1st of the month falls on a wednesday
-    // [tr,tr,tr,tr]
-    // tr = [td,td,td,td,td,td,td]
     populateDates() {
         console.log('populateDates');
-        let allWeeks = [];
-        let week = [];
-        let dateNumber = 1; // date counter
-        let weekNumber = 1; // first week
-        let lastFullDate = this.getLastFullDate();
-        let lastDate = lastFullDate.getDate();
-        let weekCount = this.getNumberOfWeeks();
-        let firstDay = this.getFirstDay();
-        let tdCount = weekCount * 7;
-        let dateCount = 0; //date counter
-        // create elements inside week. always will have 7 tds.
-        for (let i = 0; i <= tdCount; i++) {
-            let elem = React.createElement('td', {key: `td-${i}`},'')
-            if(i >= firstDay && i <= firstDay + lastDate - 1) {
-                elem = React.createElement('td', {key: `td-${i}`, onClick: this.getParentData},dateNumber)
-                dateNumber++;
-            }
-            week.push(elem)
-        }
+        const allWeeks = this.createWeek();
+        const lastDate = (this.getLastFullDate()).getDate();
+        const firstDay = this.getFirstDay();
+        const allWeeksElem = allWeeks.map((week, index) => {
+            return React.createElement('tr', {id: `week-${index + 1}`, 'data-weeknumber': index + 1}, 
+                week.map((day, ind) => {
+                    if(day >= firstDay && day <= firstDay + lastDate - 1) {
+                        return React.createElement('td', {key: `td-${ind}`, onClick: this.getParentData},day)
+                    } else {
+                        return React.createElement('td', {key: `td-${ind}`}, day)
+                    }
+                    
+                })
+            );
 
-        for(let i = weekNumber; i <= weekCount; i++) {
-            let elem = React.createElement('tr', {id: `week-${i}`, 'data-weeknumber': i
-            // , ref: this[`week${i}ref`]
-        },
-            week[dateCount],
-            week[dateCount+1],
-            week[dateCount+2],
-            week[dateCount+3],
-            week[dateCount+4],
-            week[dateCount+5],
-            week[dateCount+6],
-            )
-            allWeeks.push(elem);
-            dateCount+=7;
-        }
-        return allWeeks;
+        })
+        return allWeeksElem;
     }
 
-    // getCurrentMonth() {
-    //     console.log('getCurrentMonth');
-    //     let month;
-    //     if(this.props.currentMonth !== null) {
-    //         month = this.props.currentMonth;
-    //     } else {
-    //         month = (new Date()).getMonth();
-    //         this.props.storeCurrentMonthToState(month);
-    //     }
-    //     return month;
-    // }
-
-    // getCurrentYear() {
-    //     console.log('getCurrentYear');
-    //     let year;
-    //     if(this.props.currentYear !== null) {
-    //         year = this.props.currentYear;
-    //     } else {
-    //         year = (new Date()).getFullYear();
-    //         this.props.storeCurrentYearToState(year);
-    //     }
-    //     return year;
-    // }
-
-    // getCurrentDate() {
-    //     console.log('getCurrentDate');
-    //     let date;
-    //     if(this.props.currentDate !== null) {
-    //         date = this.props.currentDate;
-    //     } else {
-    //         date = (new Date()).getDate();
-    //         this.props.storeCurrentDateToState(date);
-    //     }
-    //     return date;
-    // }
-
-
-    // // last date of any given month is
-    // // 1st date of next month - 1
-    // // need current year
-    // getLastFullDate() {
-    //     console.log('getLastFullDate');
-    //     let currentMonth = this.props.currentMonth || this.getCurrentMonth();
-    //     let currentYear = currentMonth === 11 ? // if december, make it new year
-    //         (this.props.currentYear  || this.getCurrentYear()) + 1 
-    //         : (this.props.currentYear  || this.getCurrentYear());
-    //     let nextMonth = currentMonth === 11 ? 0 : currentMonth + 1;
-    //     // add 1 to nextMonth to get exact month
-    //     // because months array is zero-based
-    //     // thus, 0 is january but 1/1 is january 1st in date format
-    //     let nextMonthFullDate = (new Date(`${nextMonth + 1}/1/${currentYear}`));
-    //     let lastFullDate =  (new Date(nextMonthFullDate.setDate(0)));
-    //     return lastFullDate;
-    // }
-
-    // // get first day of the month
-    // getFirstDay() {
-    //     console.log('getFirstDay');
-    //     let month = this.getCurrentMonth() + 1;
-    //     let currentYear = this.getCurrentYear();
-    //     let firstDate = (new Date(`${month}/1/${currentYear}`));
-    //     let firstDay = firstDate.getDay();
-    //     return firstDay;
-    // }
-
-
-    // //get # of weeks
-    // // count 2 weeks straight off-the-bat. 1st week where first date of month is.
-    // // 2nd week is where last date of month is.
-    // // take first date of the last week, subtract 1 to get the last date of the week before
-    // // i.e. if 27 is first date of last week, then the 26th is the last date of the week before
-    // // after getting that date, look for the last date of the first week
-    // // then subtract that from the last date of 2nd-to-the-last week
-    // getNumberOfWeeks() {
-    //     console.log('getNumberOfWeeks');
-    //     let weekCount = 2;
-    //     let lastFullDate = this.getLastFullDate();
-    //     let lastDate = lastFullDate.getDate();
-    //     let lastDay = lastFullDate.getDay();
-    //     let a = lastDate - lastDay;
-    //     let firstDay = this.getFirstDay();
-    //     let b = 1 + (6 - firstDay);
-    //     weekCount += (a - b - 1)/7
-    //     return weekCount;
-    // }
-
+    
     //next month and previous month
     prevMonth() {
         console.log('prevMonth');
--- client/src/components/Week.jsx
+++ client/src/components/Week.jsx
@@ -1,78 +1,95 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { getLastFullDate, getNumberOfWeeks, getFirstDay, getLastFullDate } from './Functions';
+import { getCurrentMonth, getCurrentYear, getCurrentDate, getFirstDay, getLastFullDate, getNumberOfWeeks, createWeek } from './Functions';
 
 class Week extends React.Component {
     constructor(props) {
         super(props);
-        this.cloneThis = this.cloneThis.bind(this);
+        // this.cloneThis = this.cloneThis.bind(this);
         this.getCurrentWeek = this.getCurrentWeek.bind(this);
+
+        this.getCurrentMonth = getCurrentMonth.bind(this);
+        this.getCurrentYear = getCurrentYear.bind(this);
+        this.getCurrentDate = getCurrentDate.bind(this);
+        this.getFirstDay = getFirstDay.bind(this);
+        this.getLastFullDate = getLastFullDate.bind(this);
+        this.getNumberOfWeeks = getNumberOfWeeks.bind(this);
+
+        this.createWeek = createWeek.bind(this);
     }
 
     componentDidMount() {
         //    this.cloneThis();
             console.log('week props', this.props.currentWeek);
-            this.week = getCurrentWeek();
-            this.cloneThis(this.week);
+            this.week = this.getCurrentWeek();
+            this.getCurrentWeek = this.getCurrentWeek.bind(this);
+            // this.cloneThis(this.week);
             // if(this.props.currentWeek === null) {
             //     this.getCurrentWeek();
             // }
-            
+        
     }
 
     getCurrentWeek() {
         console.log('getCurrentWeek');
-        let week;
-        let saturdays = [];
-        const lastDate = (getLastFullDate()).getDate();
+        // let week;
+        // let saturdays = [];
+        // const lastDate = (getLastFullDate()).getDate();
         const currentDate = (new Date()).getDate();
-        const day = (new Date()).getDay();
-        const weekCount = getNumberOfWeeks();
-        let sat = 1;
-        // get first saturday
-        let firstOfDay = getFirstDay();
-        let lastDate = (getLastFullDate()).getDate();
-        let sat1 = 1 + (6 - firstOfMonthDay);
-        saturdays.push(sat1);
-        sat = sat1;
-        for(let i = 2; i <= weekCount; i++) {
-            sat+=7;
-            saturdays.push(sat);
-        }
-        saturdays.forEach( (elem) => {
-            
-        })
+        // const day = (new Date()).getDay();
+        // const weekCount = getNumberOfWeeks();
+        // let sat = 1;
+        // // get first saturday
+        // let firstOfDay = getFirstDay();
+        // // let lastDate = (getLastFullDate()).getDate();
+        // let sat1 = 1 + (6 - firstOfMonthDay);
+        // saturdays.push(sat1);
+        // sat = sat1;
+        // for(let i = 2; i <= weekCount; i++) {
+        //     sat+=7;
+        //     saturdays.push(sat);
+        // }
+        // saturdays.forEach( (elem) => {
         
+        // })
 
-        
+        const week = this.createWeek();
+        let weekNumber = week.findIndex((arr) => {
+            return arr === (arr.find((d) => {
+                return d === currentDate
+            }) ? arr : 'error' )
 
-        
+        }) + 1;
 
+        console.log('weeknumber', weekNumber);
+
+        return weekNumber;
+    
     }
 
-    getWeekElem() {
-        console.log('getWeekElem');
-        let week;
-        if(this.props.currentWeek === null) {
-            
-        } else {
-            console.log(Array.from(document.querySelectorAll('#calendar td')).find(el => el.textContent === this.getCurrentDate()));
-            week = (Array.from(document.querySelectorAll('#calendar td')).find(el => el.textContent === this.getCurrentDate())).parentNode.id;
-            week = 'week-2';
-            this.props.storeCurrentWeekToState('week-2');
-        }
-
-        // this.props.storeCurrentWeekToState(week);
-        // return week;
+    // getWeekElem() {
+    //     console.log('getWeekElem');
+    //     let week;
+    //     if(this.props.currentWeek === null) {
         
-    }
+    //     } else {
+    //         console.log(Array.from(document.querySelectorAll('#calendar td')).find(el => el.textContent === this.getCurrentDate()));
+    //         week = (Array.from(document.querySelectorAll('#calendar td')).find(el => el.textContent === this.getCurrentDate())).parentNode.id;
+    //         week = 'week-2';
+    //         this.props.storeCurrentWeekToState('week-2');
+    //     }
 
-    cloneThis(week) {
-            let c = document.getElementById('copy');
-            c.appendChild((document.getElementById('weekdays')).cloneNode(true));
-            c.appendChild((document.getElementById('week-2')).cloneNode(true));
-    }
+    //     // this.props.storeCurrentWeekToState(week);
+    //     // return week;
+    
+    // }
+
+    // cloneThis(week) {
+    //         let c = document.getElementById('copy');
+    //         c.appendChild((document.getElementById('weekdays')).cloneNode(true));
+    //         c.appendChild((document.getElementById('week-2')).cloneNode(true));
+    // }
 
     render() {
         return (
@@ -82,10 +99,12 @@ class Week extends React.Component {
                         <tr>
                             <th colSpan='7'>
                                 Week
+                                
                             </th>
                         </tr>
                     </thead>
                     <tbody id='copy'>
+                    {this.week}
                     </tbody>
                </table>
             </div>
