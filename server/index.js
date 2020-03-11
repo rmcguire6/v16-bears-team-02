6c0b67652bad3b974fc9e61df9617997ca9ded5a
Regina Mc Guire <rmcguire@antelecom.net>
2020-03-10 17:11:30 -0700

    Add participant router

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const db = require('./models')
 const meetingRouter = require('./routers/meeting')
+const participantRouter = require('./routers/participant')
 
 db.sequelize.authenticate()
     .then(() => console.log('Database connected ...'))
@@ -9,6 +10,7 @@ db.sequelize.authenticate()
 const app = express()
 app.use(express.json())
 app.use(meetingRouter)
+app.use(participantRouter)
 
 port = process.env.PORT || 5000
 app.listen(port, () => {
